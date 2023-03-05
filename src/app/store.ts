7f6fc1f48f19d6cd83306b45ed3eae13dc0c6968
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-03-06 02:53:38 +0300

    fix (store) added new middleware

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,15 +1,30 @@
 import {Action, combineReducers, configureStore, ThunkAction} from '@reduxjs/toolkit';
-import {starWarsApi} from "../API/SWService";
+import {planetAPI} from "../api/PlanetService";
 import {setupListeners} from "@reduxjs/toolkit/query";
+import {peopleAPI} from "../api/PeopleService";
+import {filmsAPI} from "../api/FilmsService";
+import {starshipsAPI} from "../api/StarshipsService";
+import {vehiclesAPI} from "../api/VehiclesService";
+import {speciesAPI} from "../api/SpeciesService";
 
 const rootReducer = combineReducers({
-    [starWarsApi.reducerPath]: starWarsApi.reducer
+    [planetAPI.reducerPath]: planetAPI.reducer,
+    [peopleAPI.reducerPath]: peopleAPI.reducer,
+    [filmsAPI.reducerPath]: filmsAPI.reducer,
+    [starshipsAPI.reducerPath]: starshipsAPI.reducer,
+    [vehiclesAPI.reducerPath]: vehiclesAPI.reducer,
+    [speciesAPI.reducerPath]: speciesAPI.reducer,
 })
 
 export const store = configureStore({
     reducer: rootReducer,
     middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(starWarsApi.middleware)
+        getDefaultMiddleware().concat(planetAPI.middleware,
+            peopleAPI.middleware,
+            filmsAPI.middleware,
+            starshipsAPI.middleware,
+            vehiclesAPI.middleware,
+            speciesAPI.middleware,)
 });
 
 export type AppDispatch = typeof store.dispatch;
