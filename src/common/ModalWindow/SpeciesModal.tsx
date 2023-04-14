be9212a6f75f959927b676cc2a8abf2a220ac98f
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-04-14 22:56:43 +0300

    feat (Modal) added SpeciesModal.tsx

---
+++ src/common/ModalWindow/SpeciesModal.tsx
@@ -0,0 +1,40 @@
+import React, {FC} from 'react';
+import {ISpecies} from "../../types/ISpecies";
+import {BasicModal} from "./BasicModal";
+import {Modal} from "react-bootstrap";
+
+interface SpeciesModalType {
+    open: boolean
+    closeHandler: () => void
+    species: ISpecies
+}
+
+export const SpeciesModal: FC<SpeciesModalType> = ({open, closeHandler, species}) => {
+    return (
+        <BasicModal open={open} closeHandler={closeHandler}>
+            <Modal.Header>
+                <Modal.Title>{species.name}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <div>
+                    <ul>
+                        <li>Classification: {species.classification}</li>
+                        <li>Designation: {species.designation}</li>
+                        <li>Average height: {species.average_height}</li>
+                        <li>Skin colors: {species.skin_colors}</li>
+                        <li>Hair colors: {species.hair_colors}</li>
+                        <li>Eye colors: {species.eye_colors}</li>
+                        <li>Average lifespan: {species.average_lifespan}</li>
+                        <li>Homeworld: {species.homeworld}</li>
+                        <li>Language: {species.language}</li>
+                        <li>People: {species.people}</li>
+                        <li>Films: {species.films}</li>
+                        <li>Created: {species.created}</li>
+                        <li>Edited: {species.edited}</li>
+                    </ul>
+                </div>
+            </Modal.Body>
+        </BasicModal>
+    );
+};
+
