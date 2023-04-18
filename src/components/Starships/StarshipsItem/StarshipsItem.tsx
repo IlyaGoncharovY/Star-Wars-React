3d32cae827dc28f4ea163b717ddb5210ea7dc39d
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-04-18 19:00:56 +0300

    fix (StarshipsModal) test peoples - data

--- src/components/Starships/StarshipsItem/StarshipsItem.tsx
+++ src/components/Starships/StarshipsItem/StarshipsItem.tsx
@@ -12,14 +12,16 @@ const StarshipsItem: FC<StarshipsItemType> = ({starships}) => {
 
     const {openModal, openModalHandler, closeModalHandler} = useOpenModal()
 
+
     return (
         <div>
             {starships.name}
             <Button onClick={openModalHandler}>Reed More</Button>
             <StarshipsModal
-            open={openModal}
-            closeHandler={closeModalHandler}
-            starships={starships}
+                open={openModal}
+                closeHandler={closeModalHandler}
+                starships={starships}
+                id={"13"}
             />
         </div>
     );
