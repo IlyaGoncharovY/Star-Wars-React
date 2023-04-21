47f5440c7ce3ba78e75a7bbaad42acb28f1624ca
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-04-21 23:57:40 +0300

    fix (FilmItem) added FilmModal.tsx

--- src/components/Films/FilmsItem/FilmItem.tsx
+++ src/components/Films/FilmsItem/FilmItem.tsx
@@ -1,15 +1,25 @@
 import React, {FC} from 'react';
 import {IFilms} from "../../../types/IFilms";
+import {Button} from "react-bootstrap";
+import {FilmModal} from "../../../common/ModalWindow/FilmModal/FilmModal";
+import {useOpenModal} from "../../../common/hookForOpenModal/useOpenModal";
 
 interface FilmItemType {
     films: IFilms
 }
 
 export const FilmItem: FC<FilmItemType> = ({films}) => {
+
+    const {openModal, openModalHandler, closeModalHandler} = useOpenModal()
+
     return (
         <div>
             {films.title}
-            <button>Reed More</button>
+            <Button onClick={openModalHandler}>Reed More</Button>
+            <FilmModal
+                open={openModal}
+                closeHandler={closeModalHandler}
+                films={films}/>
         </div>
     );
 };
