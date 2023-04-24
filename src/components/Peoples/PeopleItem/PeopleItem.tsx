8438230c71b8035fbeb280af71ef7aed08cf061d
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-04-24 17:42:20 +0300

    fix (PeopleItem) use PeopleModal.tsx

--- src/components/Peoples/PeopleItem/PeopleItem.tsx
+++ src/components/Peoples/PeopleItem/PeopleItem.tsx
@@ -1,15 +1,25 @@
 import React, {FC} from 'react';
 import {IPeople} from "../../../types/IPeople";
+import {useOpenModal} from "../../../common/hookForOpenModal/useOpenModal";
+import {Button} from "react-bootstrap";
+import {PeopleModal} from "../../../common/ModalWindow/PeopleModal/PeopleModal";
 
 interface PeopleItemType {
     people: IPeople
 }
 
 const PeopleItem: FC <PeopleItemType> = ({people}) => {
+
+    const {openModal, openModalHandler, closeModalHandler} = useOpenModal()
+
     return (
         <div>
             {people.name}
-            <button>Reed More</button>
+            <Button onClick={openModalHandler}>Reed More</Button>
+            <PeopleModal
+                open={openModal}
+                closeHandler={closeModalHandler}
+                people={people}/>
         </div>
     );
 };
